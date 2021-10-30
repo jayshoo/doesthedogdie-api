882b2960b12c02bd4e43f7a1f1ea74664ad0759a
Joshua McLean <jayshoo@gmail.com>
2021-10-31 04:37:09 +1100

    first commit

---
+++ doesthedogdie.ts
@@ -0,0 +1,70 @@
+const DOESTHEDOGDIE_APIKEY: string = Deno.env.get('DOESTHEDOGDIE_APIKEY')!
+
+interface Media {
+  id: number
+  name: string
+  releaseYear: string
+  itemType: { id: number, name: string }
+  genre: string
+  numRatings: number
+  updatedAt: string // but really timestamp 2021-10-19T23:12:14.000Z
+}
+
+interface Topic {
+  /** @example a plane crashes */
+  name: string
+  /** @example no plane crashes */
+  notName: string
+  /** @example Does a plane crash */
+  doesName: string
+  /** @example where a plane crashes */
+  listName: string
+  /** @example planes crashing */
+  smmwDescription: string 
+  description?: string
+  keywords?: string
+  isSpoiler: boolean
+  isSensitive: boolean
+  isVisible: boolean
+}
+
+interface TopicItem {
+  topic: Topic
+  isYes: number
+  yesSum: number
+  noSum: number
+  comment?: string // Someone can get shot in the head through the eye. Guns are used to shoot at monsters.
+}
+
+interface MediaStats {
+  item: Media
+  topicItemStats: TopicItem[]
+}
+
+export async function search(query: string): Promise<Media[]> {
+  let url = new URL('https://www.doesthedogdie.com/dddsearch')
+  url.searchParams.set('q', query)
+  
+  let result = await fetch(url, {
+    headers: {
+      'accept': 'application/json',
+      'X-API-KEY': DOESTHEDOGDIE_APIKEY
+    }
+  })
+
+  return result.json().then(a => a.items)
+}
+
+export async function detail(id: string | number): Promise<MediaStats> {
+  let url = new URL(`https://www.doesthedogdie.com/media/${id}`)
+  
+  let result = await fetch(url, {
+    headers: {
+      'accept': 'application/json',
+      'X-API-KEY': DOESTHEDOGDIE_APIKEY
+    }
+  })
+
+  return result.json()
+}
+
---
+++ main.ts
@@ -0,0 +1,33 @@
+import { search, detail } from './doesthedogdie.ts'
+
+async function ddd(query: string): Promise<Response> {
+  let searchResult = await search(query)
+  if (!searchResult)
+    return new Response(`Couldn't find ${query}. Soz`)
+
+  let videogameResult = searchResult.find(a => a.itemType.name == 'Video Game')
+  if (!videogameResult)
+    return new Response(`Couldn't find ${query}. Soz`)
+  
+  let details = await detail(videogameResult.id)
+  if (!details)
+    return new Response(`Couldn't load DDD for ${videogameResult.name} {id=${videogameResult.id}}. Soz`)
+
+  let dogDetails = details.topicItemStats.find(a => a.topic.name == 'a dog dies')
+  if (!dogDetails)
+    return new Response(`DDD had no dog-related info for ${videogameResult.name} {id=${videogameResult.id}}`)
+  
+  if (dogDetails.isYes) {
+    return new Response(`Sorry... ${dogDetails.topic.name} in ${details.item.name} (${details.item.releaseYear}).`)
+  } else {
+    return new Response(`Phew... ${dogDetails.topic.name} in ${details.item.name} (${details.item.releaseYear}).`)
+  }
+}
+
+addEventListener('fetch', event => {
+  let path = decodeURIComponent(new URL(event.request.url).pathname.slice(1))
+  if (path.startsWith('favicon.ico')) return event.respondWith(new Response('', { status: 404 }))
+  
+  console.log('fetch listener:', path)
+  event.respondWith(ddd(path))
+})
